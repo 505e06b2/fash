20b3156c5f5bd6cf03d418aa8b4cba1a03ffd951
505e06b2 <505e06b2@gmail.com>
2023-02-26 21:50:24 +0000

    Move default variables out of __init__ Move if callable check out of try/except

--- fash/profile/variables.py
+++ fash/profile/variables.py
@@ -20,49 +20,51 @@ class VariablesEnum(object):
 class _Variables(dict):
 	def __getitem__(self, key):
 		try:
-			ret = super().__getitem__(key)
-			if callable(ret):
-				ret = ret()
-
-			return ret
+			ret = super().__getitem__(key.upper())
 
 		except KeyError:
 			return ""
 
+		if callable(ret):
+			ret = ret()
+
+		return ret
+
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 
-		self[VariablesEnum.shell] = "fash"
-		self[VariablesEnum.PS1] = r"<bold><ansibrightred>\u</ansibrightred>@<ansibrightred>\h</ansibrightred></bold>:<bold><ansicyan>\w</ansicyan>\g</bold>\$ "
-		self[VariablesEnum.PS1_git_format] = " <ansibrightred>(%s)</ansibrightred>"
-		self[VariablesEnum.home] = lambda: os.environ.get("HOME") or os.environ.get("USERPROFILE") or None #not using pathlib.Path.expanduser allows the use of HOME on Windows in CPython 3.8+
-		self[VariablesEnum.username] = lambda: os.environ.get("USER") or os.environ.get("USERNAME") or "?"
-		self[VariablesEnum.hostname] = lambda: platform.node() or os.environ.get("COMPUTERNAME") or "?"
-
-		self[VariablesEnum.completion_style] = CompleteStyle.READLINE_LIKE
-
-		#To find the keys for the following dict, in Python REPL:
-		"""
-		from prompt_toolkit.document import Document
-		from prompt_toolkit.lexers import PygmentsLexer
-		from pygments.lexers.shell import BashLexer
-		PygmentsLexer(BashLexer).lex_document(Document("[test line]"))(0)
-		"""
-		#made to support white on black, 16 colours - loosely following the default for naming: https://github.com/pygments/pygments/blob/master/pygments/styles/default.py
-		self[VariablesEnum.default_style] = Style.from_dict({
-			"pygments.keyword": "ansibrightgreen",
-			"pygments.name.builtin": "ansibrightgreen",
-			"pygments.name.variable": "ansibrightcyan",
-			"pygments.literal.string": "ansibrightyellow",
-			"pygments.literal.string.escape": "ansibrightblue",
-
-			"shell.symlink": "ansicyan bold",
-			"shell.directory": "ansiblue bold",
-			"shell.executable": "ansigreen bold",
-			"shell.file": "default"
-		})
-
 Variables = _Variables()
+
+#default variables
+Variables[VariablesEnum.shell] = "fash"
+Variables[VariablesEnum.PS1] = r"<bold><ansibrightred>\u</ansibrightred>@<ansibrightred>\h</ansibrightred></bold>:<bold><ansicyan>\w</ansicyan>\g</bold>\$ "
+Variables[VariablesEnum.PS1_git_format] = " <ansibrightred>(%s)</ansibrightred>"
+Variables[VariablesEnum.home] = lambda: os.environ.get("HOME") or os.environ.get("USERPROFILE") or None #not using pathlib.Path.expanduser allows the use of HOME on Windows in CPython 3.8+
+Variables[VariablesEnum.username] = lambda: os.environ.get("USER") or os.environ.get("USERNAME") or "?"
+Variables[VariablesEnum.hostname] = lambda: platform.node() or os.environ.get("COMPUTERNAME") or "?"
+
+Variables[VariablesEnum.completion_style] = CompleteStyle.READLINE_LIKE
+
+#To find the keys for the following dict, in Python REPL:
+"""
+from prompt_toolkit.document import Document
+from prompt_toolkit.lexers import PygmentsLexer
+from pygments.lexers.shell import BashLexer
+PygmentsLexer(BashLexer).lex_document(Document("[test line]"))(0)
+"""
+#made to support white on black, 16 colours - loosely following the default for naming: https://github.com/pygments/pygments/blob/master/pygments/styles/default.py
+Variables[VariablesEnum.default_style] = Style.from_dict({
+	"pygments.keyword": "ansibrightgreen",
+	"pygments.name.builtin": "ansibrightgreen",
+	"pygments.name.variable": "ansibrightcyan",
+	"pygments.literal.string": "ansibrightyellow",
+	"pygments.literal.string.escape": "ansibrightblue",
+
+	"shell.symlink": "ansicyan bold",
+	"shell.directory": "ansiblue bold",
+	"shell.executable": "ansigreen bold",
+	"shell.file": "default"
+})
 
 try:
 	from .user_variables import *
