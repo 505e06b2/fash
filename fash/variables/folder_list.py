f7ae03a399dfa265174f5a6c1aa797dd9838a67c
505e06b2 <505e06b2@gmail.com>
2023-02-28 04:39:20 +0000

    Keep 2 different separators depending on environment for FolderList - Add len property

--- fash/variables/folder_list.py
+++ fash/variables/folder_list.py
@@ -5,23 +5,27 @@ from ..path import Path
 class FolderList:
 	@property
 	def string(self):
-		return self._separator.join(self._store)
+		return self._default_separator.join(self._store)
+
+	@property
+	def len(self):
+		return len(self._store)
 
 	def __repr__(self):
 		if sys.platform == "win32":
-			return self._separator.join([Path.expand(collapsed_path) for collapsed_path in self._store])
+			return self._windows_separator.join([Path.expand(collapsed_path) for collapsed_path in self._store])
 
 		else:
 			return self.string
 
 	def __init__(self, original_str):
 		self._store = []
+		self._default_separator = ":"
+		self._windows_separator = ";"
 
 		if sys.platform == "win32":
-			self._separator = ";"
-			for win_path in original_str.strip(self._separator).split(self._separator):
+			for win_path in original_str.strip(self._windows_separator).split(self._windows_separator):
 				self._store.append(Path.collapse(win_path))
 
 		else:
-			self._separator = ":"
-			self._store = original_str.strip(self._separator).split(self._separator)
+			self._store = original_str.strip(self._default_separator).split(self._default_separator)
