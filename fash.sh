8067b974c7afb584b4ada8ba33e7d673477a8290
505e06b2 <505e06b2@gmail.com>
2023-03-04 13:45:29 +0000

    Slight changes to launcher script - comments added after testing with cygwin

--- fash.sh
+++ fash.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-export SHELL="$(realpath -- "$0")"
+#this allows sudo to work on genuine Linux
+#to use cygwin+gsudo with fash, you must not have fash on a network drive, then type `gsudo "$SHELL"` - this may be handy in an alias
+export SHELL="$(readlink -m -- "$0")"
+
 PYTHONPATH="$(dirname -- "$SHELL")" exec python -m fash
