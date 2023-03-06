015f2d45f452ea4de922a0e37cb675328171ebe4
505e06b2 <505e06b2@gmail.com>
2023-03-06 17:49:19 +0000

    Alter platform warning message to be more specific

--- fash/platform.py
+++ fash/platform.py
@@ -1,7 +1,8 @@
 import sys
 
 if sys.platform == "win32":
-	print("Native Win32 is not supported. Please install MSYS2 or Cygwin")
+	print("Native Win32 is not supported. Please install Python3 in MSYS2 or Cygwin")
+	input("Press ENTER to exit")
 	sys.exit(1)
 
 is_cygwin = (sys.platform == "cygwin")
